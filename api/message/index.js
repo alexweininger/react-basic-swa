8770a190100c0678a42389e138852c9d556b8e98
alexweininger <alex.weininger@live.com>
2021-09-08 14:38:42 -0700

    edit workflow file

--- api/message/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = async function (context, req) {
-  context.log('JavaScript HTTP trigger function processed a request.');
-
-  context.res.json({
-    text: 'Hello there from lala API'
-  });
-};
